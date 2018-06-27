b236a70d0aee0c7c056a803d7d0a0b5e28475585
Tom Castle <minifigone@gmail.com>
2018-06-26 20:22:33 -0400

    currently generating a matrix of random values between 0 and 21. next step, checking neighbors

--- src/main.rs
+++ src/main.rs
@@ -8,36 +8,40 @@ struct Point {
     z: i32,
 }
 
-impl Point {
-    fn set_y(&mut self, yval: i32){
-        self.y = yval;
-    }
-}
+// impl Point {
+//     fn set_y(&mut self, yval: i32){
+//         self.y = yval;
+//     }
+// }
 
 fn main() {
-    let mut r1: Vec<Point> = Vec::new();
-    let mut r2: Vec<Point> = Vec::new();
-    let mut r3: Vec<Point> = Vec::new();
+    let r1: Vec<Point> = Vec::new();
+    let r2: Vec<Point> = Vec::new();
+    let r3: Vec<Point> = Vec::new();
+    let r4: Vec<Point> = Vec::new();
+    let r5: Vec<Point> = Vec::new();
 
     let mut m: Vec<Vec<Point>> = Vec::new();
 
     m.push(r1);
     m.push(r2);
     m.push(r3);
+    m.push(r4);
+    m.push(r5);
 
     let mut j = 0;
 
     for mut r in m {
-        for i in 1..=3 {
+        for i in 1..=10 {
             r.push(Point {
                 x: j,
-                y: rand::thread_rng().gen_range(-10, 11),
+                y: rand::thread_rng().gen_range(0, 20),
                 z: i,
             });         
         }
 
         for p in &r {
-            print!("{} ", p.y.to_string());
+            print!("{:03} ", p.y.to_string());
         }
         println!("");
 
